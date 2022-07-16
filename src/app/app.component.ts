3152f6e0e110b90a5524e28674bcb001c1b3f020
Francesco Leardini <francesco.leardini@mobi.ch>
2022-07-16 15:53:25 +0200

    Added code for registration form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
+import { RegistrationForm } from './model/registration.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular Workspace';
+
+  registrationForm!: FormGroup<RegistrationForm>;
+
+  get username() {
+    return this.registrationForm.get('username');
+  }
+
+  constructor(private fb: NonNullableFormBuilder) { }
+
+  ngOnInit(): void {
+    this.registrationForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required]
+    });
+  }
+
+  register() {
+    console.log(this, this.registrationForm.value);
+  }
 }
