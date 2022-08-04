1ac8457adb418a8a127ec15cd377febf8a356ad6
Francesco Leardini <francesco.leardini@mobi.ch>
2022-08-04 18:02:40 +0200

    Removed duplicate module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
